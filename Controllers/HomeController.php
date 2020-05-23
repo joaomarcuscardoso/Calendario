9ea775ec9aaf804c1bf0a9868f827eac337de9f3
joaomarcuscardoso <rfcjoujou@gmail.com>
2020-05-23 13:50:31 -0300

    Foi feito mês do dias correspodentes

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -1,31 +1,58 @@
 <?php
 class HomeController extends Controller {
-    public function __construction() {
-
+    private $array;
+    public function __construct() {
+        date_default_timezone_set('America/Sao_Paulo');
+        $this->array = array("month" => array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Juno", "Julho", "Agosto", "Setembro", "Outubro", "Novembro","Dezembro"),
+        "month_number" => date("n"), "year" => date("yy"));
+        
+        
+        
     }
 
     public function index() {
-        $array = array();
+        $data = new Data();  
+
+        $this->array['data'] = $data->selectAll();
 
 
-        $array['month'] = array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Juno", "Julho", "Agosto", "Setembro", "Outubro", "Novembro","Dezembro");
-        $data = new Data();   
-
-        if(!empty($_POST['months'])) {
-
-            $months = addslashes($_POST['months']);
-            
-/*             header("Location: "+BASE_URL+$months);
-            exit; */
-        }
 
         $months = date("m");
+       
         $now = getdate(strtotime($months));
 
-        $array["months"] = cal_days_in_month(CAL_GREGORIAN, $now['mon'], $now['year']);
-        $array['data'] = $data->selectAll();
+        $this->array["months"] = cal_days_in_month(CAL_GREGORIAN, $now['mon'], $now['year']);
+        
 
-        $this->loadTemplate("home", $array);
+        $this->loadTemplate("home", $this->array);
+    }
+
+
+    public function change_month() {
+        if(!empty($_POST["month-value"]) && !empty($_POST['year-value'])) {
+
+            $months = addslashes($_POST['month-value']); 
+            $this->array['year'] = intval(addslashes($_POST['year-value']));
+
+
+            foreach($this->array['month'] as $key => $values) {
+                if($values == $months) {
+                    $this->array['month_number'] =   $key + 1;
+                } 
+            }
+           
+
+            
+            $this->array["months"] = cal_days_in_month(CAL_GREGORIAN, $this->array['month_number'], $this->array['year']);
+            $this->loadTemplate("home", $this->array);
+
+        } else {
+            header("Location: ".BASE_URL);
+            exit;
+        } 
+
+        
+        
     }
 
 
--- Views/home.php
+++ Views/home.php
@@ -1,24 +1,25 @@
 <div class="row" id="container-month">
-    <div class="col-sm-2 month-container-prev"><a href="#"  class="btn btn-outline-info btn-arrow" id="month-prev"   >&#10094;</a></div>
-    <div class="col-sm-8">
-        <div class="month">
-            
-            <span id="month-letter"><?php echo $month[date("n")- 1]; ?> de <?php echo date("yy"); ?></span>
-            <input type="hidden" id="month-value" value="<?php echo $month[date("n") -1] ;?>" />
-            <input type="hidden" id="year-value" value="<?php echo date("yy"); ?>" />
-        </div>
-    </div>
-    <div class="col-sm-2 month-container-next"><a href="" class="btn btn-outline-info btn-arrow" id="month-next" >&#10095;</a></div>
 
+        <form method="POST" action="<?php echo BASE_URL; ?>home/change_month" id="calendar-form">
+            <input type="hidden" name="month-value" id="month-value" value="<?php echo $month[$month_number -1] ;?>" />
+            <input type="hidden" name="year-value" id="year-value" value="<?php echo $year; ?>" />
+        </form>
+        <div class="col-sm-2 month-container-prev"><a href="#"  class="btn btn-outline-info btn-arrow" id="month-prev"   >&#10094;</a></div>
+        <div class="col-sm-8">
+            <div class="month">
+                
+                <span id="month-letter"><?php echo $month[$month_number- 1]; ?> de <?php echo $year; ?></span>
+
+            </div>
+        </div>
+        <div class="col-sm-2 month-container-next"><a href="" class="btn btn-outline-info btn-arrow" id="month-next" >&#10095;</a></div>
+   
 </div>
 
 <div class="row" style="margin:0px!important;">
     <div class="col-sm-12 calendar-col">
         <div class="calendar-container">
-            <form method="POST" id="calendar-form">
-                <input type="hidden" id="calendar-container-value" name="months" value="<?php echo $month[date("m")]; ?>" />
 
-            </form>
 
             
             <?php for($i = 0;$i <= $months;$i++): // 42 ?>
